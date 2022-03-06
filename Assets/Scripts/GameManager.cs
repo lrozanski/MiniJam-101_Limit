ff8e32936ab121adfee8c74897464184399b8e08
Lech Różański <l.rozanski88@gmail.com>
2022-03-06 17:24:41 +0100

    Added a game overlay

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using LR.Core;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+public class GameManager : SingletonMonoBehaviour<GameManager> {
+
+    [field: SerializeField, PropertyRange(0f, 100f)]
+    public int Lives { get; set; }
+
+    private void Update() {
+        if (Lives <= 0) {
+            LoseGame();
+        }
+    }
+
+    private void LoseGame() {
+        
+    }
+
+}
--- Assets/Scripts/Map/FinishLine.cs
+++ Assets/Scripts/Map/FinishLine.cs
@@ -1,13 +1,15 @@
-﻿using AI;
+﻿using System;
+using AI;
 using LR.Core.Utils;
 using Sirenix.OdinInspector;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Map {
     public class FinishLine : MonoBehaviour {
 
         private const int Rows = 3;
-        
+
         [SerializeField, SceneObjectsOnly]
         private Transform carContainer;
 
@@ -26,38 +28,25 @@ namespace Map {
         [SerializeField, PropertyRange(1, 10), BoxGroup("Enemy Properties")]
         private int enemyCars;
 
+        private Rect spawnArea;
+        
         private void Start() {
+            var bounds = boxCollider2D.bounds;
+            spawnArea = new Rect(bounds.min.x, bounds.min.y, bounds.size.x, bounds.size.y);
+            
             SpawnEnemies();
         }
 
         private void SpawnEnemies() {
-            var bounds = boxCollider2D.bounds;
-            var rect = new Rect(bounds.min.x, bounds.min.y, bounds.size.x, bounds.size.y);
-
             for (var i = 0; i < enemyCars; i++) {
-                var gridPosition = GridUtils.IndexTo2DCoords(i, Rows);
-                var localPosition = new Vector2(gridPosition.y, gridPosition.x);
-                var position = rect.min + Vector2.one * 0.5f + localPosition;
-
-                var enemy = Instantiate(enemyPrefab, position, Quaternion.AngleAxis(-90f, Vector3.forward), carContainer);
-                var seedRandom = Random.Range(-skillRandom, skillRandom);
-
-                enemy.Ai = seed;
-                seed.accelerateAmount += seedRandom;
-                seed.decelerateAmount += seedRandom;
-                seed.turnAmount += seedRandom;
-
-                enemy.StartDelay = Random.value * 0.5f;
+                SpawnEnemy(i);
             }
         }
 
         public void SpawnEnemy(int index) {
-            var bounds = boxCollider2D.bounds;
-            var rect = new Rect(bounds.min.x, bounds.min.y, bounds.size.x, bounds.size.y);
-            
             var gridPosition = GridUtils.IndexTo2DCoords(index, Rows);
             var localPosition = new Vector2(gridPosition.y, gridPosition.x);
-            var position = rect.min + Vector2.one * 0.5f + localPosition;
+            var position = spawnArea.min + Vector2.one * 0.5f + localPosition;
 
             var enemy = Instantiate(enemyPrefab, position, Quaternion.AngleAxis(-90f, Vector3.forward), carContainer);
             var seedRandom = Random.Range(-skillRandom, skillRandom);
@@ -68,6 +57,12 @@ namespace Map {
             seed.turnAmount += seedRandom;
 
             enemy.StartDelay = Random.value * 0.5f;
+        }
+
+        private void OnTriggerEnter2D(Collider2D col) {
+            if (col.CompareTag("Enemy")) {
+                
+            }
         }
 
     }
